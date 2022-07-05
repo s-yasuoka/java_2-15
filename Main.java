e3c90ec44cee21c0a6ec0aa5ad870fad7471566e
s-yasuoka <migayasu@gmail.com>
2022-07-05 13:56:34 +0900

    [add]キーボード入力

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main{
   public static void main(String[] args){
-    int r = new java.util.Random().nextInt(90);
-    System.out.println("あなたは" + r + "歳ですね？");
+    System.out.println("名前を入力してください");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("年齢を入力してください");
+    int age = new java.util.Scanner(System.in).nextInt();
+    System.out.println("ようこそ" + age + "歳" + name);
   }
 }
